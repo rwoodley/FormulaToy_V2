2945a61b49e7581d98932304665338551c39987e
rwoodley <blwoodley@yahoo.com>
2014-01-29 14:08:45 -0600

    adding pinterest

--- params.js
+++ params.js
@@ -32,7 +32,7 @@ function shareFormula() {
     document.getElementById("shareTW").href = 'http://twitter.com/share?text=' + cleanFormula + '&url=' + encodeURIComponent(url);
     document.getElementById("shareFB").href = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url);
     document.getElementById("sharePI").href = "https://www.pinterest.com/pin/create/button/?url=" + encodeURIComponent(url)
-        + "&media=" + encodeURIComponent("http://3linematrix.com.s3-website-us-east-1.amazonaws.com/FormulaToy.FormulaToy" + PNGUKey + ".png");
+        + "&media=" + encodeURIComponent("http://3linematrix.com.s3-website-us-east-1.amazonaws.com/FormulaToy.FormulaToy" + pngukey + ".png");
     document.getElementById('modalBackground').style.display = 'block';
     var el = document.getElementById('ShareDiv');
 
